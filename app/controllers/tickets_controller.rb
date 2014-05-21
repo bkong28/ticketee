59b424e521ab6d4e765d6806c65f3b7f6d92d24c
Brandon Kong <bkong28@gmail.com>
2014-05-21 19:21:39 +1000

    We can now edit tickets.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -15,7 +15,20 @@ class TicketsController < ApplicationController
 	    flash[:alert] = "Ticket has not been created."
 	    render "new"
 	  end
-	end 
+	end
+
+	def edit
+	end
+
+	def update
+	  if @ticket.update(ticket_params)
+	    flash[:notice] = "Ticket has been updated."
+	    redirect_to [@project, @ticket]
+	  else
+	    flash[:alert] = "Ticket has not been updated."
+	    render action: "edit"
+	  end
+	end
 
 	private
 	  def ticket_params
